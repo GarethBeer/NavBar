4cefd971702532e55ffcb9d6629c5e0ac5a41466
GarethBeer <gareth.beer1989@outlook.com>
2019-12-19 13:44:43 +0000

    responsive nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import "../Style/navbar.css";
 
-const NavBar = () => {
-	return (
-		<nav className="navbar">
-			<div className="logo">Gareth Beer</div>
-			<ul className="navlinks">
-				<li>About</li>
-				<li>Services</li>
-				<li>Projects</li>
-				<li>Contact</li>
-			</ul>
-			<div class="burger">
-				<div className="line1"></div>
-				<div className="line2"></div>
-				<div className="line3"></div>
-			</div>
-		</nav>
-	);
-};
+class NavBar extends Component {
+	state = {
+		sideBar: false
+	};
 
+	handleBurgerClick = () => {
+		if (!this.state.sideBar) {
+			this.setState({
+				sideBar: true
+			});
+		} else {
+			this.setState({ sideBar: false });
+		}
+	};
+
+	render() {
+		const { sideBar } = this.state;
+
+		return (
+			<nav className="navbar">
+				<div className="logo">Gareth Beer</div>
+				<ul className={`navlinks ${sideBar ? "active" : ""}`}>
+					<li>About</li>
+					<li>Services</li>
+					<li>Projects</li>
+					<li>Contact</li>
+				</ul>
+				<div className="burger" onClick={this.handleBurgerClick}>
+					<div className="line1"></div>
+					<div className="line2"></div>
+					<div className={`line3 ${sideBar ? "active" : ""}`}></div>
+				</div>
+			</nav>
+		);
+	}
+}
 export default NavBar;
